9b4e7043f76a21b2c3a994d106d431e31fa9b12e
蕭孟杰 <b06605026@g.ntu.edu.tw>
2024-07-16 21:20:26 +0800

    發布github page設定

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Superlaty_web/' : '/',
   plugins: [react()],
 })
